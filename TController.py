f342de09fb47ce0b4ba3d087c8f733c33d724db6
David Ng <dnhkng@gmail.com>
2017-03-13 22:35:15 +0100

    Initial Commit

---
+++ TController.py
@@ -0,0 +1,42 @@
+import time
+import zmq
+import multiprocessing
+
+server_ports = {"twitter"   : 7000,
+                "facebook"  : 7001,
+                "twitch"    : 7002,
+                "other"     : 7003}
+
+def server(currentValues,name):
+    port = server_ports[name]
+    with zmq.Context() as context:
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+        socket.bind("tcp://*:%s" % port)
+        print(name + " %s running server on port: %d" % (name, port))
+        while True:
+            # Wait for next request from client
+            message = socket.recv_pyobj()
+            print("Received notification from%s: %s" % (name, message))
+            currentValues[name] = message
+            socket.send_string("Message Recieved %s" % port)
+
+if __name__ == '__main__':
+    mgr = multiprocessing.Manager()
+    currentValues = mgr.dict()
+    currentValues.update({  'twitter'       : 0, 
+                            'facebook'      : 0, 
+                            'fb_comments'   : 0,
+                            'twitch'        : 0})
+
+    server_list = [ multiprocessing.Process(target=server, 
+                            args=(currentValues, names)) for names in list(server_ports.keys())]
+    for j in server_list:
+        j.start()
+    while True:
+        print(currentValues)
+        time.sleep(1)
+
+
+
+
---
+++ test.py
@@ -0,0 +1,9 @@
+import zmq
+import random
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect ("tcp://localhost:7001")
+socket.send_pyobj(random.randint(0, 10))
+message = socket.recv()
+print ("Received reply ", "[", message, "]")
