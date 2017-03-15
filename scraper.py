16036cbfbdea4caf476f2346742cdd61efea81ce
David Ng <dnhkng@gmail.com>
2017-03-15 21:59:29 +0100

    Twitter Streaming code

---
+++ scraper.py
@@ -0,0 +1,49 @@
+import settings
+import tweepy
+#import dataset
+from textblob import TextBlob
+import json
+
+class StreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        
+
+        if status.retweeted:
+            return
+        print(status.text.encode('utf-8'))
+        print(status.entities['hashtags'])
+        description = status.user.description
+        loc = status.user.location
+        text = status.text
+        coords = status.coordinates
+        geo = status.geo
+        name = status.user.screen_name
+        user_created = status.user.created_at
+        followers = status.user.followers_count
+        id_str = status.id_str
+        created = status.created_at
+        retweets = status.retweet_count
+        bg_color = status.user.profile_background_color
+        blob = TextBlob(text)
+        sent = blob.sentiment
+
+        if geo is not None:
+            geo = json.dumps(geo)
+
+        if coords is not None:
+            coords = json.dumps(coords)
+
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_data disconnects the stream
+            return False
+
+auth = tweepy.OAuthHandler(settings.TWITTER_APP_KEY, settings.TWITTER_APP_SECRET)
+auth.set_access_token(settings.TWITTER_KEY, settings.TWITTER_SECRET)
+api = tweepy.API(auth)
+
+stream_listener = StreamListener()
+stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+stream.filter(track=settings.TRACK_TERMS)
